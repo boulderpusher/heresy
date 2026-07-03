cc5963e98da5de76f3b9a07b9732c8f45a08b4a5
Ben Jacobsohn <ben.jacobsohn07@gmail.com>
2026-07-03 09:50:12 -0500

    shooting animation

--- marine.gd
+++ marine.gd
@@ -2,10 +2,18 @@ extends CharacterBody3D
 
 @export var speed = 10
 
-func _physics_process(_delta):
-	var animation_player = get_node("Pivot/Body/AnimationPlayer")
+var animation_player
+
+func _ready():
+	animation_player = get_node("Pivot/Body/AnimationPlayer")
 	animation_player.play("walk")
+
+func _physics_process(_delta):
 	var direction = Vector3.BACK
 	velocity = direction * speed
 	$Pivot.basis = Basis.looking_at(direction, Vector3.UP, true)
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	speed = 0
+	animation_player.play("shoot")
