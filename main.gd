97bdae876d7385528e453a322daa7f1067a08798
Ben Jacobsohn <ben.jacobsohn07@gmail.com>
2026-07-06 17:35:17 -0500

    attacking and state control

--- main.gd
+++ main.gd
@@ -7,24 +7,30 @@ extends Node
 enum Team {BLUE, RED}
 
 var marine_scene = preload("res://marine/marine.tscn")
-var _allies: Array[Marine]
-var _enemies: Array[Marine]
+var _units: Array[Marine]
 
 func _ready() -> void:
-	for i in range(n_allies):
-		var ally = marine_scene.instantiate()
-		add_child(ally)
-		ally.set_team(Team.BLUE)
-		_allies.append(ally)
-	for i in range(n_enemies):
-		var enemy = marine_scene.instantiate()
-		add_child(enemy)
-		enemy.set_team(Team.RED)
-		_enemies.append(enemy)
-	for ally in _allies:
-		ally.walk(Vector3.FORWARD.rotated(Vector3.UP, randf_range(-2*PI, 2*PI)))
-	for enemy in _enemies:
-		enemy.walk(Vector3.FORWARD.rotated(Vector3.UP, randf_range(-2*PI, 2*PI)))
+	_spawn_armies()
 
 func _process(delta: float) -> void:
 	pass
+
+func _spawn_armies():
+	var spawn_location_blue = $SpawnPathBlue/SpawnLocation
+	for i in range(n_allies):
+		spawn_location_blue.progress_ratio = float(i + 1) / float(n_allies + 1)
+		_spawn_marine(spawn_location_blue.position, Vector3(1, 0, 0), Team.BLUE)
+		
+	var spawn_location_red = $SpawnPathRed/SpawnLocation
+	for i in range(n_enemies):
+		spawn_location_red.progress_ratio = float(i + 1) / float(n_enemies + 1)
+		_spawn_marine(spawn_location_red.position, Vector3(-1, 0, 0), Team.RED)
+		
+func _spawn_marine(location, direction, team):
+	var team_group = "blue_team" if team == Team.BLUE else "red_team"
+	var marine = marine_scene.instantiate()
+	marine.initialize(location, direction)
+	add_child(marine)
+	marine.set_team(team)
+	marine.add_to_group(team_group)
+	_units.append(marine)
--- marine/marine.gd
+++ marine/marine.gd
@@ -1,47 +1,137 @@
 extends CharacterBody3D
 class_name Marine
 
-@export var speed = 10
+@export var speed: float  = 10.0
+@export var max_health: int = 100
+@export var attack_damage: int = 10
 
 var _team: Main.Team
+var _enemy_group
 var _armor_material: Material
 var _eyes_material: Material
 var _animation_player: AnimationPlayer
 var _skeleton: Skeleton3D
+var _health_bar: ProgressBar
 
+var _current_health: int
 var _direction: Vector3
-var _speed
+var _speed: float
+
+enum State {IDLE, WALKING, ATTACKING}
+
+var _state = State.IDLE
+var _target: Node3D
+var _can_attack: bool = true
 
 func _ready():
 	_skeleton = get_node("Pivot/Body/Armature/Skeleton3D")
 	_animation_player = get_node("Pivot/Body/AnimationPlayer")
-	_animation_player.play("walk")
-	_direction = Vector3.FORWARD
+	_health_bar = get_node("HealthBar")
+	_health_bar.max_value = max_health
+	_current_health = max_health
+	_health_bar.value = _current_health
+	_enemy_group = "red_team" if _team == Main.Team.BLUE else "blue_team"
+	_select_new_target()
 
+func _process(delta):
+	if _state == State.ATTACKING: # state machine deez nuts
+		_attack()
+	elif _state == State.WALKING:
+		_walk()
+	elif _state == State.IDLE:
+		_idle()
+			
 func _physics_process(_delta):
-	velocity = _direction * speed
-	$Pivot.basis = Basis.looking_at(_direction, Vector3.UP, true)
+	velocity = _direction.normalized() * _speed
 	move_and_slide()
 
-func shoot():
-	_speed = 0
-	_animation_player.play("shoot")
-	
-func walk(direction):
-	_speed = speed
-	_direction = direction
-	_animation_player.play("walk")
+func initialize(initial_position, initial_direction):
+	position = initial_position
+	_direction = initial_direction
 
+func _attack():
+	if not _target:
+		_select_new_target()
+		if not _target: # no new target available
+			return
+	_direction = _target.position - position
+	_look_at(_direction)
+	if _can_attack:
+		_animation_player.play("shoot")
+		_target.take_damage(attack_damage)
+		_can_attack = false
+		$AttackCooldown.start()
+		
+func take_damage(damage):
+	_current_health = max(0, _current_health - damage)
+	_health_bar.value = _current_health
+	if _current_health == 0:
+		die()
+		
+func die():
+	queue_free()
+	
+func _walk():
+	_direction = _target.position - position
+	_look_at(_direction)
+
+func _idle():
+	pass
+
+func _look_at(direction):
+	$Pivot.basis = Basis.looking_at(direction, Vector3.UP, true)
+
+func _set_state(new_state: State):
+	_state = new_state
+	if _state == State.WALKING:
+		_speed = speed
+		_animation_player.play("walk")
+	elif _state == State.ATTACKING:
+		_speed = 0
+		_animation_player.play("shoot")
+	elif _state == State.IDLE:
+		_speed = 0
+		_animation_player.play("idle")
+
+func _select_new_target():
+	var enemies = $AttackRange.get_overlapping_bodies()
+	if not enemies:
+		enemies = get_tree().get_nodes_in_group(_enemy_group)
+		_target = _get_nearest(enemies)
+		if  _target:
+			_set_state(State.WALKING)
+		else:
+			_set_state(State.IDLE)
+	else:
+		_target = _get_nearest(enemies)
+		_set_state(State.ATTACKING)
+		
+func _get_nearest(bodies):
+	var min_distance = INF 
+	var nearest = null
+	for body in bodies:
+		var distance = position.distance_to(body.position)
+		if distance < min_distance:
+			min_distance = min_distance
+			nearest = body
+	return nearest
+
+# store team and change armor color
 func set_team(team: Main.Team):
 	if team == Main.Team.BLUE:
 		_team = Main.Team.BLUE
 		_armor_material = load("res://marine/materials/armor_blue.tres")
 		_eyes_material = load("res://marine/materials/eyes_red.tres")
+		set_collision_layer_value(2, true)
+		$AttackRange.set_collision_mask_value(3, true)
+
 	if team == Main.Team.RED:
 		_team = Main.Team.RED 
 		_armor_material = load("res://marine/materials/armor_red.tres")
 		_eyes_material = load("res://marine/materials/eyes_green.tres")
-		
+		set_collision_layer_value(3, true)
+		$AttackRange.set_collision_mask_value(2, true)
+
 	for bone in _skeleton.get_children():
 		if bone is not BoneAttachment3D:
 			continue
@@ -55,3 +145,12 @@ func set_team(team: Main.Team):
 					mesh_instance.set_surface_override_material(surf_idx, _armor_material)
 				if material.resource_name == "eyes_base":
 					mesh_instance.set_surface_override_material(surf_idx, _eyes_material)
+
+func _on_attack_cooldown_timeout() -> void:
+	_can_attack = true
+
+func _on_attack_range_body_entered(body: Node3D) -> void:
+	if _state != State.ATTACKING:
+		_set_state(State.ATTACKING)
+		_target = body
+		
